4189cbdfa0eca633a84f79d8ec1e0011ca99495e
Benedikt Rötsch <info@benedikt-roetsch.de>
2014-12-12 12:49:46 +0100

    lints javascript and applies coding standards

--- js/pollanon.js
+++ js/pollanon.js
@@ -1,23 +1,24 @@
+'use strict';
 (function ($) {
   Drupal.behaviors.pollanonHandleVoteView = {
     attach: function (context) {
       //Does this page contain poll form or poll results?
-      if (typeof PollAnon == 'undefined' || !PollAnon.nid) {
+      if (typeof PollAnon === 'undefined' || !PollAnon.nid) {
         return;
       }
 
-      cookieName = 'pa-' + PollAnon.nid;
+      var cookieName = 'pa-' + PollAnon.nid;
 
       //Form element is hidden by CSS to prevent flashing on switching to result display
       $('form.pollanon').fadeIn('fast');
 
       if ($.cookie(cookieName)) {
         //Cookie exists: This anonymous user has already submitted the given poll
-        $hiddenResults = $('.pollanon-poll-results.hidden');
+        var $hiddenResults = $('.pollanon-poll-results.hidden');
         if ($hiddenResults.length > 0) {
           //Hide poll form options and button
-          $poll_form = $('form.pollanon .vote-form');
-          $('.form-radios, .form-submit', $poll_form).hide();
+          var $pollForm = $('form.pollanon .vote-form');
+          $('.form-radios, .form-submit', $pollForm).hide();
           //Display results
           $hiddenResults.hide(); //Needs to be hidden by jQuery for the fadeIn() to work
           $hiddenResults.removeClass('hidden'); //Remove CSS hiding
@@ -26,17 +27,17 @@
       }
       else {
         //User might be submitting the poll form
-        ua = navigator.userAgent;
-        uaI = Math.floor(ua.length/2);
-        pollanonKey = ua? ua.substring(uaI, uaI+2) + ua.length : '';
+        var ua = navigator.userAgent;
+        var uaI = Math.floor(ua.length/2);
+        var pollanonKey = ua ? ua.substring(uaI, uaI+2) + ua.length : '';
         pollanonKey += '-' + new Date().getTime();
         $.cookie('pa-submit', pollanonKey, {path: '/'});
         $('form.pollanon input[name="pollanonkey"]', context).attr('value', pollanonKey);
       }
 
-      msg = $.cookie('pa-messages');
+      var msg = $.cookie('pa-messages');
       if (msg) {
-        msg = unescape(msg.replace(/\+/g, " "));
+        msg = unescape(msg.replace(/\+/g, ' '));
         $('form.pollanon').before('<div class="messages status">'+msg+'</div>');
         $.cookie('pa-messages', null, { path: '/' }); //Remove message cookie
       }
